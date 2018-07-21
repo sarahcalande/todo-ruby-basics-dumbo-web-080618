61d9976d70dd7b9b0bc65bf1bd0d12c32c120ce7
scalande@yahoo.com <scalande@yahoo.com>
2018-07-21 17:07:54 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -7,21 +7,21 @@ def assign_variable(value)
 end
 
 def argue
-  
+  argue
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-
+puts "Nice"
 end
 
 def last_evaluated_value
-
+puts "expert"
 end
 
-def pizza_party
-
+def pizza_party(food = "cheese")
+food
 end
